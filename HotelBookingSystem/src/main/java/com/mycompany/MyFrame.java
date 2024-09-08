33ca13c41252cb6106a2f369c7794cb15e7a9507
Kate <kake0898@gmail.com>
2024-09-08 22:20:36 +1200

    Add the User login options on start up

--- HotelBookingSystem/src/main/java/com/mycompany/MyFrame.java
+++ HotelBookingSystem/src/main/java/com/mycompany/MyFrame.java
@@ -13,6 +13,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,26 +27,87 @@ import javax.swing.border.TitledBorder;
  */
 public class MyFrame extends JFrame{
     
+    JPanel mainPanel;
+    JPanel loginPanel;
+    JPanel startPanel;
+    CardLayout cards;
+    
+    
     //Constructor
     public MyFrame () {
+
+        init();
+        loadStaffLogin();
+        loadMenuPanel();
+        
+        cards.show(mainPanel, "STARTPANEL");
+        this.add(mainPanel);
+        setVisible(true);
+        
+    }
+     
+    //initializes frame
+    private void init() {
         setSize(640,400);
         setLocationRelativeTo(null);
         setTitle("Hotel Booking System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         //MAIN PANEL
-        JPanel mainPanel = new JPanel();
+        mainPanel = new JPanel();
         mainPanel.setBorder(new TitledBorder("MAIN PANEL"));
-        CardLayout cards = new CardLayout();
+        cards = new CardLayout();
         mainPanel.setLayout(cards);
         
-        JLabel label1 = new JLabel("Label1");
-        mainPanel.add(label1, "LABEL1");
+        //Start Up Panel
+        startPanel = new JPanel();
+        startPanel.setLayout(new BorderLayout());
+        startPanel.setBorder(new TitledBorder("LOGIN OPTIONS"));
         
+        //User-OPTIONS COMPONENTS
+        JPanel userOptionComponents = new JPanel();
+        userOptionComponents.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        JLabel labelOptions = new JLabel("Select User: ");
+        userOptionComponents.add(labelOptions, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 2;
+        String[] comboOptions = new String[] {"Login as Guest", "Login as Staff"};
+        JComboBox<String> userComboBox = new JComboBox<String>(comboOptions);
+        userOptionComponents.add(userComboBox, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        JButton button1 = new JButton("Continue");
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cards.show(mainPanel, "LOGINPANEL");
+            }            
+        });
+        userOptionComponents.add(button1, gbc);
+        
+        startPanel.add(userOptionComponents, BorderLayout.CENTER);
+        mainPanel.add(startPanel, "STARTPANEL");
+       
+    }
+    
+    private void loadStaffLogin () {
         //LOGIN PANEL
-        JPanel loginPanel = new JPanel();
-        loginPanel.setLayout(new BorderLayout());       
-        
+        loginPanel = new JPanel();
+        loginPanel.setLayout(new BorderLayout());            
         loginPanel.setBorder(new TitledBorder("LOGIN PANEL"));
+        
         //LOGIN COMPONENTS
         JPanel loginComponents = new JPanel();
         loginComponents.setLayout(new GridBagLayout());
@@ -97,8 +159,9 @@ public class MyFrame extends JFrame{
         loginPanel.add(loginComponents, BorderLayout.CENTER);
 //        loginPanel.add(loginComponents, BorderLayout.CENTER);
         mainPanel.add(loginPanel, "LOGINPANEL");
-        
-        
+    }
+    
+    private void loadMenuPanel() {
         //MAINMENU PANEL
         JPanel menuPanel = new JPanel();
         menuPanel.setBorder(new TitledBorder("MENU PANEL"));
@@ -112,12 +175,5 @@ public class MyFrame extends JFrame{
         });
         menuPanel.add(button2, "BUTTON2");
         mainPanel.add(menuPanel, "MENUPANEL");
-        
-        
-        cards.show(mainPanel, "LOGINPANEL");
-        this.add(mainPanel);
-        setVisible(true);
-        
     }
-    
 }
