ed7d7e351c387822cf49aa32d44d14c9fa03690e
Kate Galela <kake0898@gmail.com>
2024-09-11 22:29:21 +1200

    Reformated loadManageBookingPanel from loadMenu function

--- HotelBookingSystem/src/main/java/com/mycompany/MyFrame.java
+++ HotelBookingSystem/src/main/java/com/mycompany/MyFrame.java
@@ -39,6 +39,7 @@ public class MyFrame extends JFrame{
     JPanel startPanel;
     JPanel menuPanel;
     CardLayout cards;
+    Integer bookingMode; //1:MANAGE BOOKING 2:MANAGE GUEST 3:MANAGE ROOMS
     String strLoginMode;
     
     
@@ -55,7 +56,7 @@ public class MyFrame extends JFrame{
         
     }
      
-    //initializes frame
+    //initializes frame -- need to encapsulate? init - main, startup  atc.  - different functions
     private void init() {
         setSize(740,500);
         setLocationRelativeTo(null);
@@ -67,6 +68,7 @@ public class MyFrame extends JFrame{
         mainPanel.setBorder(new TitledBorder("MAIN PANEL"));
         cards = new CardLayout();
         mainPanel.setLayout(cards);
+        bookingMode = -1; 
         
         //Start Up Panel
         startPanel = new JPanel();
@@ -189,30 +191,57 @@ public class MyFrame extends JFrame{
             }            
         });
         loginComponents.add(button1, gbc);
-//        loginPanel.add(button1, BorderLayout.SOUTH);
         
         
         loginPanel.add(loginComponents, BorderLayout.CENTER);
-//        loginPanel.add(loginComponents, BorderLayout.CENTER);
         mainPanel.add(loginPanel, "LOGINPANEL");
     }
     
+    
+    
     private void loadMenuPanel() {
-        //MAINMENU PANEL
         menuPanel = new JPanel();
-//        menuPanel.setBorder(new TitledBorder("MENU PANEL"));
         menuPanel.setLayout(new BorderLayout());   
-        
-        
-        
-        
-        //TabbedPane has tabPanel1 (manage bookings), tabPanel2 (manage guests), tabPanel3(manage rooms)
+               
+        //TabbedPane has tabPanel1, tabPanel2, tabPanel3
         JTabbedPane menuComponents = new JTabbedPane();
-        JPanel tabPanel1 = new JPanel();
-        tabPanel1.setLayout(new BorderLayout());
-        //tabPanel1.setLayout(new GridLayout(1, 2));
-        //Buttons for manage bookings menu
+        //MANAGE BOOKINGS
+        JPanel tabPanel1 = new JPanel();               
+        //MANAGE GUESTS
+        JPanel tabPanel2 = new JPanel();
+        //MANAGE ROOMS
+        JPanel tabPanel3 = new JPanel();
+                
+        menuComponents.add("Manage Bookings", tabPanel1);
+        menuComponents.add("Manage Guests", tabPanel2);
+        menuComponents.add("Manage Rooms", tabPanel3);
+        loadManageBookingsPanel(tabPanel1);
+        loadManageGuests(tabPanel2);
+        loadManageRooms(tabPanel3);
         
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
+        JButton btnLogout = new JButton("Logout");
+        btnLogout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cards.show(mainPanel, "LOGINPANEL");
+            }
+        });
+        bottomPanel.add(btnLogout);
+        
+        menuPanel.add(bottomPanel, BorderLayout.SOUTH);
+        menuPanel.add(menuComponents, BorderLayout.CENTER);
+        mainPanel.add(menuPanel, "MENUPANEL");
+        
+    }
+    
+    
+    private void loadManageBookingsPanel(JPanel tabPanel1) {
+        
+        tabPanel1.setLayout(new BorderLayout());
+
+        //Buttons for manage bookings menu on LEFT
         JPanel leftPanel1 = new JPanel();
         leftPanel1.setLayout(new BorderLayout());
         
@@ -239,27 +268,30 @@ public class MyFrame extends JFrame{
         makeBookingbtn.addActionListener(new ActionListener() {          
             @Override
             public void actionPerformed(ActionEvent e) {  
-                GridBagConstraints gbc = new GridBagConstraints();
-                JLabel label1 = new JLabel("Enter Booking Details");
-                JLabel guestNameLabel = new JLabel("Guest Name: ");
-                JTextField guestNametxf = new JTextField(10);
-                JLabel numLabel = new JLabel("Phone Number: ");
-                JTextField numTxf = new JTextField(10);
-                JButton addBtn = new JButton("Submit");
-                gbc.anchor = GridBagConstraints.WEST;
-                gbc.gridx = 0; gbc.gridy = 0; gbc.gridheight = 1; gbc.gridwidth = 1; 
-                leftPanel1btnCENTER.add(label1, gbc);
-                gbc.gridx = 0; gbc.gridy = 1; gbc.gridheight = 1; gbc.gridwidth = 1;
-                leftPanel1btnCENTER.add(guestNameLabel, gbc);
-                gbc.gridx = 1; gbc.gridy = 1; gbc.gridheight = 1; gbc.gridwidth = 1;
-                leftPanel1btnCENTER.add(guestNametxf,gbc);
-                gbc.gridx = 0; gbc.gridy = 2; gbc.gridheight = 1; gbc.gridwidth = 1;
-                leftPanel1btnCENTER.add(numLabel, gbc);
-                gbc.gridx = 1; gbc.gridy = 2; gbc.gridheight = 1; gbc.gridwidth = 1;
-                leftPanel1btnCENTER.add(numTxf, gbc);
-                gbc.gridx = 1; gbc.gridy = 3; gbc.gridheight = 1; gbc.gridwidth = 1;
-                leftPanel1btnCENTER.add(addBtn, gbc);
-                loadMenuPanel();
+                if (bookingMode != 1) {
+                    bookingMode = 1;
+                    GridBagConstraints gbc = new GridBagConstraints();
+                    JLabel label1 = new JLabel("Enter Booking Details");
+                    JLabel guestNameLabel = new JLabel("Guest Name: ");
+                    JTextField guestNametxf = new JTextField(10);
+                    JLabel numLabel = new JLabel("Phone Number: ");
+                    JTextField numTxf = new JTextField(10);
+                    JButton addBtn = new JButton("Submit");
+                    gbc.anchor = GridBagConstraints.WEST;
+                    gbc.gridx = 0; gbc.gridy = 0; gbc.gridheight = 1; gbc.gridwidth = 1; 
+                    leftPanel1btnCENTER.add(label1, gbc);
+                    gbc.gridx = 0; gbc.gridy = 1; gbc.gridheight = 1; gbc.gridwidth = 1;
+                    leftPanel1btnCENTER.add(guestNameLabel, gbc);
+                    gbc.gridx = 1; gbc.gridy = 1; gbc.gridheight = 1; gbc.gridwidth = 1;
+                    leftPanel1btnCENTER.add(guestNametxf,gbc);
+                    gbc.gridx = 0; gbc.gridy = 2; gbc.gridheight = 1; gbc.gridwidth = 1;
+                    leftPanel1btnCENTER.add(numLabel, gbc);
+                    gbc.gridx = 1; gbc.gridy = 2; gbc.gridheight = 1; gbc.gridwidth = 1;
+                    leftPanel1btnCENTER.add(numTxf, gbc);
+                    gbc.gridx = 1; gbc.gridy = 3; gbc.gridheight = 1; gbc.gridwidth = 1;
+                    leftPanel1btnCENTER.add(addBtn, gbc);
+                    loadMenuPanel();
+                }
             }
         });
         JButton checkInBtn = new JButton("Check In");       
@@ -286,51 +318,15 @@ public class MyFrame extends JFrame{
         //Where the splitpane is located along the middle of two panes
         tabPanel1Inner.setResizeWeight(.5d); 
         tabPanel1.add(tabPanel1Inner, BorderLayout.CENTER);
-        
-        
-        
-        JPanel tabPanel2 = new JPanel();
-        tabPanel2.setLayout(new BorderLayout());
-        
-        JPanel tabPanel3 = new JPanel();
-        tabPanel3.setLayout(new BorderLayout());
-                
-        menuComponents.add("Manage Bookings", tabPanel1);
-        menuComponents.add("Manage Guests", tabPanel2);
-        menuComponents.add("Manage Rooms", tabPanel3);
-        loadManageBookings(tabPanel1);
-        loadManageGuests(tabPanel2);
-        loadManageRooms(tabPanel3);
-        
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setLayout(new FlowLayout(FlowLayout.TRAILING));
-        JButton btnLogout = new JButton("Logout");
-        btnLogout.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cards.show(mainPanel, "LOGINPANEL");
-            }
-        });
-        bottomPanel.add(btnLogout);
-        
-        menuPanel.add(bottomPanel, BorderLayout.SOUTH);
-        menuPanel.add(menuComponents, BorderLayout.CENTER);
-        mainPanel.add(menuPanel, "MENUPANEL");
-        
     }
-    
     
     //JSplitPane
     //JList
-    private void loadManageBookings(JPanel bookingsPanel) {
-        
+    private void loadManageGuests(JPanel tabPanel2) {
+        tabPanel2.setLayout(new BorderLayout());
     }
     
-    private void loadManageGuests(JPanel guestsPanel) {
-        
-    }
-    
-    private void loadManageRooms(JPanel roomsPanel) {
-        
+    private void loadManageRooms(JPanel tabPanel3) {
+        tabPanel3.setLayout(new BorderLayout());
     }
 }
